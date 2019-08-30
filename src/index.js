9d341620b546fbc361ba1dc806b6036609db8041
vovchyk <klischvolodimir@gmail.com>
2019-08-31 00:31:06 +0300

    sdf

--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(5000, ()=> console.log("Express server is listening on port 5000"));
+app.listen(2047, ()=> console.log("Express server is listening on port 2047"));
