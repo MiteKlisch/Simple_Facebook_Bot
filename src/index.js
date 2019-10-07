63a6eb950f16488789b45f5db33911f58361fa2a
vovchyk <klischvolodimir@gmail.com>
2019-10-07 14:33:55 +0300

    maybe working bot

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const verifyWebhook = require('./verify-webhook');
-app.get('/', verifyWebhook);
+
+require('dotenv').config({ path: 'variables.env' });
+const messageWebhook = require('./message-webhook');
+
+
 
 const app = express();
 
+app.get('/', verifyWebhook);
+app.post('/', messageWebhook);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3520, ()=> console.log("Express server is listening on port 5000"));
+app.listen(5000, ()=> console.log("Express server is listening on port 5000"));
