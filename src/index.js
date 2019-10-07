d4b2172c953633ed5de034bf7df592d74b5d0731
Volodymyr <klischvolodimir@gmail.com>
2019-10-07 18:56:52 +0300

    asds

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const verifyWebhook = require('./verify-webhook');
-app.get('/', verifyWebhook);
+
 
 const app = express();
+
+app.get('/', verifyWebhook);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3520, ()=> console.log("Express server is listening on port 5000"));
+app.listen(5000, ()=> console.log("Express server is listening on port 5000"));
--- src/verify-webhook.js
+++ src/verify-webhook.js
@@ -1,14 +1,15 @@
 const verifyWebhook = (req, res) => {
-    let VERIFY_TOKEN = 'pusher-bot'
+    const VERIFY_TOKEN = 'pusher-bot'
 
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
 
     if (mode && token === VERIFY_TOKEN) {
         res.status(200).send(challenge);
     }   else {
         res.sendStatus(403);
+        
     }
 };
 
