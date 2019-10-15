73b991e9e80e5d8df2efaa2a417d2ed6b3dc7d66
martabobyk <martysuk99@gmail>
2019-10-15 23:44:02 +0300

    improved webhook verification logic

--- src/verify-webhook.js
+++ src/verify-webhook.js
@@ -1,19 +1,21 @@
 require('dotenv').config({ path: 'variables.env' });
 
-
 const verifyWebhook = (req, res) => {
-    
+
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
-    if (mode && token === variables.env.VERIFY_TOKEN) {
-        res.status(200).send(challenge);
-    }   else {
-        res.sendStatus(403);
-        console.log('Problem with token');
+    if (mode && token) {
+        if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
-    
 };
 
 module.exports = verifyWebhook;
