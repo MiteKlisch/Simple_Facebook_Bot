953194a073bf7d45d04bedabdb2e94bd6ad83701
martabobyk <martysuk99@gmail>
2019-10-15 23:43:02 +0300

    webhook endpoints and PORT as process variable

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 require('dotenv').config({ path: 'variables.env' });
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const verifyWebhook = require('./verify-webhook');
-
-
 const messageWebhook = require('./message-webhook');
-
 
 
 const app = express();
 
-app.get('/', verifyWebhook);
-app.post('/', messageWebhook);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(5000, ()=> console.log("Express server is listening on port 5000"));
+app.get('/home', (req, res) => { res.status(200).send('Hi, friend! Happy you are using my app') });
+
+app.get('/webhook', verifyWebhook);
+app.post('/webhook', messageWebhook);
+
+app.listen(process.env.PORT, ()=> console.log(`Express server is listening on port ${process.env.PORT}`));
